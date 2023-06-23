41643dc5e424f05cb96b4bc8cc11672989525153
smathis <mathis.silotia@ynov.com>
2023-06-23 14:53:01 +0200

    change import router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Home from './pages/Home';
-import { BrowserRouter as Router, Routes, Route }
+import { HashRouter as Router, Routes, Route }
     from 'react-router-dom';
 
 function App() {
